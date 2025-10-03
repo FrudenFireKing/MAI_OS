cebe072c81eeaf2d0c4e42fed35578e71ae88ac6
FrudenFireKing <fedorgorbacev942@gmail.com>
2025-10-03 21:44:58 +0300

    let's go

---
+++ lab_1/src/client.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <stdio.h>
---
+++ lab_1/src/server.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+    
+}
